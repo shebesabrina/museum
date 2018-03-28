76752cbd32ad5681d265421af480376e8b65f91a
Sabrina Robinson <sabrinarob@gmail.com>
2018-03-28 09:12:53 -0600

    Iteration completed with patron having interests.

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -5,11 +5,25 @@ require 'pry'
 
 class PatronTest < Minitest::Test
   def test_it_exists
-    patron = Patron.new("Bob")
+    bob = Patron.new("Bob")
   end
 
   def test_it_has_a_name
-    patron = Patron.new("Bob")
-    assert_equal "Bob", patron.name
+    bob = Patron.new("Bob")
+    assert_equal "Bob", bob.name
+  end
+
+  def test_bob_starts_with_interests
+    bob = Patron.new("Bob")
+    assert_equal [], bob.interests
+  end
+
+  def test_bob_has_multiple_interests
+    bob = Patron.new("Bob")
+    bob.add_interest("Dead Sea Scrolls")
+    bob.add_interest("Gems and Minerals")
+    
+    result = ["Dead Sea Scrolls", "Gems and Minerals"]
+    assert_equal result, bob.interests
   end
 end
